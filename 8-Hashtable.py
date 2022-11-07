4072f4eb9cc9063a36154a5aabcc60280519265e
Roshan John <91770248+RoshanJ20@users.noreply.github.com>
2022-11-07 14:35:58 +0530

    Update 8-Hashtable.py

--- 8-Hashtable.py
+++ 8-Hashtable.py
@@ -1,23 +1,39 @@
-from typing import List
+import re
 
-def insertionSort(array) -> List[int]:
-  # Write your code here
-  for i in range(1, len(array)):
-    key = array[i]
-    j = i - 1
-    while j>=0 and key < array[j]:
-      array[j+1] = array[j]
-      j -= 1
-    array[j+1] = key
-    
-  return array
+def display_hash(hashtable) -> None:
+	"""
+	Displays elements stored at each position of the hash table.
+	"""
+	for keyvalue in range(len(hashtable)):
+		content = hashtable[keyvalue]
+		print(keyvalue, end = " ")
+		for element in content:
+			print("-->", element, end = " ")
+		print()
 
-# data = [9, 5, 1, 4, 3]
+def Hashing(keyvalue) -> int:
+	"""
+	Returns the ouput of the hash function for the given key value.
+	"""
+	return keyvalue % len(HashTable)
+
+def insert(Hashtable, keyvalue, value) -> None:
+	"""
+	Inserts the value at the position of the hastable returned by applying the hash function to keyvalue.
+	"""
+	Hashtable[Hashing(keyvalue)].append(value)
+
+
+# Do not edit the following code
+hash_table_size = int(input())
+# Create Hashtable as a list of list.
+HashTable = [[] for _ in range(hash_table_size)]
 input_data = input()
 data = []
-for item in input_data.split(', '):
-  if item.isnumeric():
-    data.append(int(item))
-  elif item.lstrip("-").isnumeric():
-    data.append(int(item))
-print(insertionSort(data))
+for item in re.split('], |].', input_data):
+  item = item[1:]
+  data = item.split(', ')
+  if len(data) > 1:
+    insert(HashTable, int(data[0]), data[1])
+
+display_hash(HashTable)
